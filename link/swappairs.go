29011ee8dd21a152eb1ac9dfdd353ab72bfb3cb5
liyantao <358225063@qq.com>
2019-06-13 16:07:55 +0800

    init

---
+++ link/swappairs.go
@@ -0,0 +1,25 @@
+//链表操作
+// 24 两两交换链表中的节点
+
+package link
+
+ type ListNode struct {
+     Val int
+     Next *ListNode
+ }
+
+
+ //递归实现
+ //思路: 两两分组 。相互交换，交换后每组的第二个的next 接受下一组的第一个。
+ func swapPairs(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {
+		return head
+	}
+	temp1 := head   
+	temp2 := head.Next
+
+	temp1.Next = swapPairs(temp2.Next)  // 每组交换后的第二个 
+	temp2.Next = temp1
+
+	return temp2
+}
